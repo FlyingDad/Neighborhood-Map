ff7a6c2e6e7c54665320d76ec19caefda349f821
Michael Kroth <mkroth65@gmail.com>
2018-07-18 08:58:09 -0500

    Options filtering working

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortby from 'sort-by'
 
 
 class Options extends Component {
@@ -6,27 +9,45 @@ class Options extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			venues: this.props.venueInfo
+			venues: this.props.venueInfo,
+			query: ''
 		}
 	}
 
 	// componentDidMount(){
 	// 	this.setState({venues: this.props.venueInfo})
 	// }
+  updateQuery = (query) => {
+    this.setState({query: query.trim()})
+  }
 
+  clearQuery = () => {
+    this.setState({query: ''})
+  }
 
 	render() {
 		//console.log(this.state.venues)
+		const {query} = this.state
+		//debugger
+    let Venues
+    if (query) {
+      const match = new RegExp(escapeRegExp(this.state.query), 'i')
+      Venues = this.state.venues.filter((venue) => match.test(venue.name))
+    } else {
+      Venues = this.state.venues
+    }
+    Venues.sort(sortby('name'))
 		return(
 			<div>
 				
 				{/* <button onClick={this.props.resetMap}>Reset Map</button> */}
 				<div>
-					<input id='search' type='text' placeholder='Search for restaurant' autoComplete='off'/>
+					<input id='search' type='text' placeholder='Search for restaurant' autoComplete='off'
+					onChange={(event)=> this.updateQuery(event.target.value)}/>
 				</div>
 				<div>
 					<div>
-						{this.state.venues.map((e) => 
+						{Venues.map((e) => 
 							<p key={e.name}>{e.name}</p>
 						)}
 					</div>
